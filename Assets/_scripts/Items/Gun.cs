e52e38816f2b1c2d06a85ddc3ca328132d60c836
menno <run_jongejan@hotmail.com>
2015-06-10 10:13:50 +0200

    Start making gun

---
+++ Assets/_scripts/Items/Gun.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour, IWeapon {
+	private float _ammo;
+	private float _reloadTime;
+	private float _currentReloadTime;
+	private float _shootCooldown;
+	private float _currentShootCooldown;
+	public virtual void Shoot()
+	{
+
+	}
+	public virtual void Reload()
+	{
+
+	}
+	public float ammo
+	{
+		set{
+			_ammo = value;
+		}
+		get{
+			return _ammo;
+		}
+	}
+	public float reloadTime
+	{
+		set{
+			_reloadTime = value;
+		}
+		get{
+			return _reloadTime;
+		}
+	}
+	public float currentReloadTime
+	{
+		set{
+			_currentReloadTime = value;
+		}
+		get{
+			return _currentReloadTime;
+		}
+	}
+	public float shootCooldown
+	{
+		set{
+			_shootCooldown = value;
+		}
+		get{
+			return _shootCooldown;
+		}
+	}
+	public float currentShootCooldown
+	{
+		set{
+			_currentShootCooldown = value;
+		}
+		get{
+			return _currentShootCooldown;
+		}
+	}
+}
---
+++ Assets/_scripts/Items/IWeapon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IWeapon {
+	void Shoot();
+	void Reload();
+	float ammo{
+		set;
+		get;
+	}
+	float reloadTime{
+		set;
+		get;
+	}
+	float currentReloadTime{
+		set;
+		get;
+	}
+	float shootCooldown{
+		set;
+		get;
+	}
+	float currentShootCooldown{
+		set;
+		get;
+	}
+}
